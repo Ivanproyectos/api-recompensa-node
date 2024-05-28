031193acc5dbfc2cfab3376dacc4764347e90497
Ivanproyectos <ivansperezt@gmail.com>
2024-05-28 18:22:22 -0500

    se borro lo transpilado de ts para probar

--- build/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_service_1 = require("../services/user.service");
-const authRouter = (0, express_1.Router)();
-authRouter.post('/auth/login', user_service_1.validarUsuario);
-exports.default = authRouter;
--- build/routes/index.route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const buscadoRoute = (0, express_1.Router)();
-buscadoRoute.get('/', (req, res) => res.send('Hello World!'));
-exports.default = buscadoRoute;
