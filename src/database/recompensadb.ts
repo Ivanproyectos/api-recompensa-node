7ee74d51326c0c89b6b2120be9ad6edbf5b6a5f3
Ivanproyectos <ivansperezt@gmail.com>
2025-01-21 21:57:06 -0500

    se cambio la conexion a base de datos de mysql a postgress

--- src/database/recompensadb.ts
+++ src/database/recompensadb.ts
@@ -2,9 +2,16 @@ import { Sequelize } from 'sequelize'
 import { dbConfig } from '../config'
 
 export const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  port: dbConfig.port,
   host: dbConfig.host,
-  dialect: 'mysql',
+  port: dbConfig.port,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,          // Obliga el uso de SSL
+      rejectUnauthorized: false, // Acepta certificados no verificados
+    },
+  },
   define: {
     underscored: true // Esta opción habilita la conversión automática de camelCase a snake_case
   }
